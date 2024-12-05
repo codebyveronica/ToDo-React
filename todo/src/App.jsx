28bcb687628a007b667052f36cbbf425f74d4d15
Veronica <codebyveronica@gmail.com>
2024-12-05 14:40:30 -0300

    add sort functionality
    I added two buttons with the functionality to sort the task list ascending and descending

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -27,6 +27,8 @@ function App() {
 
   const [filter, setFilter] = useState("All");
 
+  const [sort, setSort] = useState("Asc");
+
   const addToDo = (text, category) => {
     const newTodos = [
       ...todos,
@@ -61,7 +63,7 @@ function App() {
     <div>
       <h1>Just do it.</h1>
       <Search search={search} setSearch={setSearch} />
-      <Filter filter={filter} setFilter={setFilter} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
         {todos
           .filter((todo) =>
@@ -73,6 +75,11 @@ function App() {
           )
           .filter((todo) =>
             todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) =>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
           )
           .map((todo) => (
             <ToDo
